947c37c4a0182ea1db67317f5b29f6486c9cd5f9
Jason A. Fager <jafager@jafager.com>
2018-02-25 23:56:32 -0500

    Turning on paging kills it

--- initial_exercises/02-long_mode/startup.asm
+++ initial_exercises/02-long_mode/startup.asm
@@ -10,6 +10,12 @@ bits 32
 extern stack_top
 extern serial_init
 extern serial_puts
+extern l4_page_table_begin
+extern l4_page_table_end
+extern l3_page_table_begin
+extern l3_page_table_end
+extern l2_page_table_begin
+extern l2_page_table_end
 
 
 
@@ -76,6 +82,73 @@ startup:
     mov eax, .message_long_mode_is_available
     call serial_puts
 
+    ; initialize l4 page table
+    mov eax, l4_page_table_begin
+    .next_l4_page_table_entry:
+    mov dword [eax], 0
+    add eax, 4
+    cmp eax, l4_page_table_end
+    jl .next_l4_page_table_entry
+
+    ; initialize l3 page table
+    mov eax, l3_page_table_begin
+    .next_l3_page_table_entry:
+    mov dword [eax], 0
+    add eax, 4
+    cmp eax, l3_page_table_end
+    jl .next_l3_page_table_entry
+
+    ; initialize l2 page table
+    mov eax, l2_page_table_begin
+    .next_l2_page_table_entry:
+    mov dword [eax], 0
+    add eax, 4
+    cmp eax, l2_page_table_end
+    jl .next_l2_page_table_entry
+
+    ; set up identity paging for l2 page table
+    mov eax, l2_page_table_begin
+    mov edx, 0b10000011
+    .next_l2_page_table_map:
+    mov dword [eax], edx
+    add eax, 8
+    add edx, 0x200000
+    cmp eax, l2_page_table_end
+    jl .next_l2_page_table_map
+
+    ; point first entry in l4 page table to the l3 page table
+    mov eax, l3_page_table_begin
+    or eax, 0b11
+    mov [l4_page_table_begin], eax
+
+    ; point first entry in l3 page table to the l2 page table
+    mov eax, l2_page_table_begin
+    or eax, 0b11
+    mov [l3_page_table_begin], eax
+
+    ; point MMU to l4 page table
+    mov eax, l4_page_table_begin
+    mov cr3, eax
+
+    ; enable PAE
+    mov eax, cr4
+    or eax, 1 << 5
+    mov cr4, eax
+
+    ; enable long mode
+    mov ecx, 0xC0000080
+    rdmsr
+    or eax, 1 << 8
+    wrmsr
+
+    ; enable paging
+    mov eax, cr0
+    or eax, 1 << 31
+    mov cr0, eax
+    hlt
+    mov eax, .message_paging_enabled
+    call serial_puts
+
     ; write the ready message to the serial port
     mov eax, .message_ready
     call serial_puts
@@ -123,3 +196,7 @@ startup:
 .message_long_mode_is_available:
 
     db `Long mode is available.\r\n\0`
+
+.message_paging_enabled:
+
+    db `Paging enabled.\r\n\0`
